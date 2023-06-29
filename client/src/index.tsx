f27bcecf0a93a9092fba251695ae8003beb74d8a
kirill-imanuilov <kirill.imanuilov@yandex.ru>
2023-06-29 15:59:36 +0300

    style: format some code
    - Format the code of the `index.tsx` and `index.css` files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,12 +5,11 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
-
