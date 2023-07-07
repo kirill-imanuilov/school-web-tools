0cf4e0a7a48fa29f60f50ce913f1043548d97ef0
kirill-imanuilov <kirill.imanuilov@yandex.ru>
2023-07-07 12:37:46 +0300

    feat (coffee delivery form)
    - Add the possibility of writing orders data to the database on the
      server via the coffee delivery form

--- client/src/components/CoffeeDeliveryForm.tsx
+++ client/src/components/CoffeeDeliveryForm.tsx
@@ -1,4 +1,19 @@
 import '../index.css';
+import { useState } from 'react';
+
+interface orderData {
+  deliveryTime: string;
+  name: string;
+  office: number;
+  building: string;
+  coffee: string;
+  cinnamon: number;
+  lemon: number;
+  sugar: number;
+  cream: number;
+  syrop: string;
+  addition: string;
+}
 
 export function CoffeeDeliveryForm() {
   const date = new Date();
@@ -10,23 +25,82 @@ export function CoffeeDeliveryForm() {
   const startTime = '8:00';
   const closingTime = '16:00';
   var time = '';
-  if (`${minutes}`.length === 1) {
+
+  if (`${hours}`.length === 1 && `${minutes}`.length === 1) {
+    time = `0${hours}:0${minutes}`;
+  } else if (`${hours}`.length === 1) {
+    time = `0${hours}:${minutes}`;
+  } else if (`${minutes}`.length === 1) {
     time = `${hours}:0${minutes}`;
   } else {
     time = `${hours}:${minutes}`;
   }
+
+  const [deliveryTime, setDeliveryTime] = useState(time);
+  const [name, setName] = useState('');
+  const [office, setOffice] = useState('');
+  const [building, setBuilding] = useState('Старый корпус');
+  const [coffee, setCoffee] = useState('Американо');
+  const [cinnamon, setCinnamon] = useState(false);
+  const [lemon, setLemon] = useState(false);
+  const [sugar, setSugar] = useState(false);
+  const [cream, setCream] = useState(false);
+  const [syrop, setSyrop] = useState('Без сиропа');
+  const [addition, setAddition] = useState('Без дополнений');
+
+  const orderData = {
+    deliveryTime: deliveryTime,
+    name: name,
+    office: +office,
+    building: building,
+    coffee: coffee,
+    cinnamon: +cinnamon,
+    lemon: +lemon,
+    sugar: +sugar,
+    cream: +cream,
+    syrop: syrop,
+    addition: addition,
+  };
+
+  async function postOrderData(orderData: orderData) {
+    let maxId;
+    await fetch('http://localhost:8000/coffee_delivery/save_order_data/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(orderData),
+    })
+      .then((response) => response.json())
+      .then((response) => (maxId = response.max_id));
+  }
+
+  const handleSubmitButtonClick = (event: any) => {
+    event.preventDefault();
+    postOrderData(orderData);
+  };
+
   return (
     <form className='coffee-delivery-form'>
       <input
         className='coffee-delivery-form-input coffee-delivery-form-input-name'
         placeholder='ФИО'
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        required
       />
       <div className='coffee-delivery-form-top-container'>
         <input
+          type='number'
           className='coffee-delivery-form-input coffee-delivery-form-input-office'
           placeholder='Каб.'
+          min={1}
+          value={office}
+          onChange={(event) => setOffice(event.target.value)}
         />
-        <select className='coffee-delivery-form-select coffee-delivery-form-select-building'>
+        <select
+          className='coffee-delivery-form-select coffee-delivery-form-select-building'
+          value={building}
+          onChange={(event) => setBuilding(event.target.value)}
+        >
           <option>Старый корпус</option>
           <option>Новый корпус</option>
         </select>
@@ -36,8 +110,14 @@ export function CoffeeDeliveryForm() {
         type='time'
         min={time}
         max={closingTime}
+        value={deliveryTime}
+        onChange={(event) => setDeliveryTime(event.target.value)}
       />
-      <select className='coffee-delivery-form-select'>
+      <select
+        className='coffee-delivery-form-select'
+        value={coffee}
+        onChange={(event) => setCoffee(event.target.value)}
+      >
         <option>Американо</option>
         <option>Капучино</option>
         <option>Латте</option>
@@ -46,22 +126,42 @@ export function CoffeeDeliveryForm() {
         <option>Эспрессо</option>
       </select>
       <label className='coffee-delivery-form-checkbox-label'>
-        <input type='checkbox' />
+        <input
+          type='checkbox'
+          checked={cinnamon}
+          onChange={(event) => setCinnamon(event.target.checked)}
+        />
         Корица
       </label>
       <label className='coffee-delivery-form-checkbox-label'>
-        <input type='checkbox' />
+        <input
+          type='checkbox'
+          checked={lemon}
+          onChange={(event) => setLemon(event.target.checked)}
+        />
         Лимон
       </label>
       <label className='coffee-delivery-form-checkbox-label'>
-        <input type='checkbox' />
+        <input
+          type='checkbox'
+          checked={sugar}
+          onChange={(event) => setSugar(event.target.checked)}
+        />
         Сахар
       </label>
       <label className='coffee-delivery-form-checkbox-label'>
-        <input type='checkbox' />
+        <input
+          type='checkbox'
+          checked={cream}
+          onChange={(event) => setCream(event.target.checked)}
+        />
         Сливки
       </label>
-      <select className='coffee-delivery-form-select'>
+      <select
+        className='coffee-delivery-form-select'
+        value={syrop}
+        onChange={(event) => setSyrop(event.target.value)}
+      >
         <option>Без сиропа</option>
         <option>Ванильный сироп</option>
         <option>Грушевый сироп</option>
@@ -69,13 +169,21 @@ export function CoffeeDeliveryForm() {
         <option>Карамельный сироп</option>
         <option>Шоколадный сироп</option>
       </select>
-      <select className='coffee-delivery-form-select'>
+      <select
+        className='coffee-delivery-form-select'
+        value={addition}
+        onChange={(event) => setAddition(event.target.value)}
+      >
         <option>Без дополнений</option>
         <option>Печенье</option>
         <option>Шоколадка</option>
       </select>
-      {time <= closingTime && <button type='submit'>Отправить</button>}
-      {time > closingTime && (
+      {time <= closingTime && name !== '' && office !== '' && (
+        <button type='submit' onClick={handleSubmitButtonClick}>
+          Отправить
+        </button>
+      )}
+      {(time > closingTime || name === '' || office === '') && (
         <button className='submit-button-disabled' type='submit' disabled>
           Отправить
         </button>
--- server/main.py
+++ server/main.py
@@ -1,5 +1,95 @@
+import sqlite3
+import datetime
+from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+connection = sqlite3.connect("./data/db.sqlite3")
+
+
+class OrderData(BaseModel):
+    deliveryTime: str
+    name: str
+    office: int
+    building: str
+    coffee: str
+    cinnamon: int
+    lemon: int
+    sugar: int
+    cream: int
+    syrop: str
+    addition: str
+
+
+with connection:
+    cursor = connection.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS coffeeDeliveryOrdersData (
+                          id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                          date TEXT NOT NULL,
+                          orderTime TEXT NOT NULL,
+                          deliveryTime TEXT NOT NULL,
+                          isCourierSent INTEGER NOT NULL,
+                          name TEXT NOT NULL,
+                          office INTEGER NOT NULL,
+                          building TEXT NOT NULL,
+                          coffee TEXT NOT NULL,
+                          cinnamon INTEGER NOT NULL,
+                          lemon INTEGER NOT NULL,
+                          sugar INTEGER NOT NULL,
+                          cream INTEGER NOT NULL,
+                          syrop TEXT NOT NULL,
+                          addition TEXT NOT NULL
+                      );
+                   """)
+
+
+@app.post("/coffee_delivery/save_order_data/")
+async def save_order_data(order_data: OrderData):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(f"""INSERT INTO coffeeDeliveryOrdersData (date,
+                                                                 orderTime,
+                                                                 deliveryTime,
+                                                                 isCourierSent,
+                                                                 name,
+                                                                 office,
+                                                                 building,
+                                                                 coffee,
+                                                                 cinnamon,
+                                                                 lemon,
+                                                                 sugar,
+                                                                 cream,
+                                                                 syrop,
+                                                                 addition)
+                          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+                       """, (str(datetime.date.today()),
+                             str(datetime.datetime.now().time())[0:8],
+                             order_data.deliveryTime,
+                             0,
+                             order_data.name,
+                             order_data.office,
+                             order_data.building,
+                             order_data.coffee,
+                             order_data.cinnamon,
+                             order_data.lemon,
+                             order_data.sugar,
+                             order_data.cream,
+                             order_data.syrop,
+                             order_data.addition))
+        max_id = cursor.execute("""SELECT MAX(id) FROM coffeeDeliveryOrdersData;""").fetchone()[0]
+
+    return {"max_id": max_id}
 
