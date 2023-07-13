806dcb634cbcad63cf258f649a92728ff87c6cb8
kirill-imanuilov <kirill.imanuilov@yandex.ru>
2023-07-13 18:34:53 +0300

    style: format the code of the `About.tsx` file

--- client/src/components/pages/About.tsx
+++ client/src/components/pages/About.tsx
@@ -2,32 +2,16 @@ import { Link } from 'react-router-dom';
 import '../../index.css';
 
 export function AboutPage() {
-    return (
-        <div
-            className='page'
-        >
-            <div
-                className='container'
-            >
-                <h1
-                    className='container-title'
-                >
-                    О сайте
-                </h1>
-                <div
-                    className='container-content'
-                >
-                    <Link
-                        to='/'
-                        className='button-link'
-                    >
-                        <button>
-                            Назад
-                        </button>
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div className='page'>
+      <div className='container'>
+        <h1 className='container-title'>О сайте</h1>
+        <div className='container-content'>
+          <Link to='/' className='button-link'>
+            <button>Назад</button>
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-
