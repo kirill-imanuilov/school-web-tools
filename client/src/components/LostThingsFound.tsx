0b42fd1d13e9be42eea4ceaa91f8ec011bb939e9
kirill-imanuilov <kirill.imanuilov@yandex.ru>
2023-07-26 21:08:53 +0300

    feat (lost things found): add some animation

--- client/src/components/LostThingsFound.tsx
+++ client/src/components/LostThingsFound.tsx
@@ -3,6 +3,7 @@ import { ReactComponent as AddIMG } from '../IMG/add_img.svg';
 import { ReactComponent as SendIMG } from '../IMG/send_img.svg';
 import { AttachFileSquareButton } from './buttons/AttachFileSquareButton';
 import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 interface lostThingFoundData {
   thingName: string;
@@ -95,63 +96,71 @@ export function LostThingsFound() {
   };
 
   return (
-    <div className='lost-things'>
-      {isAdding === false && (
-        <button
-          type='button'
-          className='lost-things-add-button'
-          onClick={handleAddButtonClick}
-        >
-          <AddIMG className='add-img' />
-        </button>
-      )}
-      {isAdding === true && (
-        <form className='lost-things-form'>
+    <AnimatePresence>
+      <div className='lost-things'>
+        {isAdding === false && (
           <button
-            type='submit'
-            className='lost-things-send-button'
-            onClick={handleSendButtonClick}
+            type='button'
+            className='lost-things-add-button'
+            onClick={handleAddButtonClick}
           >
-            <SendIMG className='send-img' />
+            <AddIMG className='add-img' />
           </button>
-          <div className='lost-things-form-top-container'>
+        )}
+        {isAdding === true && (
+          <motion.form
+            className='lost-things-form'
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: 'auto', opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.1 }}
+          >
+            <button
+              type='submit'
+              className='lost-things-send-button'
+              onClick={handleSendButtonClick}
+            >
+              <SendIMG className='send-img' />
+            </button>
+            <div className='lost-things-form-top-container'>
+              <input
+                className='lost-things-form-item'
+                placeholder='Что Вы нашли?'
+                value={thingName}
+                onChange={(event) => setThingName(event.target.value)}
+              />
+              <AttachFileSquareButton onChange={handleAttachThingIMGChange} />
+            </div>
             <input
               className='lost-things-form-item'
-              placeholder='Что Вы нашли?'
-              value={thingName}
-              onChange={(event) => setThingName(event.target.value)}
+              placeholder='Где Вы нашли?'
+              value={thingDetectionPlace}
+              onChange={(event) => setThingDetectionPlace(event.target.value)}
             />
-            <AttachFileSquareButton onChange={handleAttachThingIMGChange} />
-          </div>
-          <input
-            className='lost-things-form-item'
-            placeholder='Где Вы нашли?'
-            value={thingDetectionPlace}
-            onChange={(event) => setThingDetectionPlace(event.target.value)}
-          />
-          <input
-            className='lost-things-form-item'
-            placeholder='Где забрать владельцу?'
-            value={thingReceiptPlace}
-            onChange={(event) => setThingReceiptPlace(event.target.value)}
-          />
-          <textarea
-            placeholder='Сообщение/описание'
-            className='lost-things-form-item lost-things-form-input-message'
-            value={userMessage}
-            onChange={(event) => setUserMessage(event.target.value)}
-          />
-          {thingIMGHTML !== '' && (
-            <div className='lost-things-form-loaded-thing-img-container'>
-              <img
-                src={thingIMGHTML}
-                width='100%'
-                className='lost-things-form-loaded-thing-img'
-              />
-            </div>
-          )}
-        </form>
-      )}
-    </div>
+            <input
+              className='lost-things-form-item'
+              placeholder='Где забрать владельцу?'
+              value={thingReceiptPlace}
+              onChange={(event) => setThingReceiptPlace(event.target.value)}
+            />
+            <textarea
+              placeholder='Сообщение/описание'
+              className='lost-things-form-item lost-things-form-input-message'
+              value={userMessage}
+              onChange={(event) => setUserMessage(event.target.value)}
+            />
+            {thingIMGHTML !== '' && (
+              <div className='lost-things-form-loaded-thing-img-container'>
+                <img
+                  src={thingIMGHTML}
+                  width='100%'
+                  className='lost-things-form-loaded-thing-img'
+                />
+              </div>
+            )}
+          </motion.form>
+        )}
+      </div>
+    </AnimatePresence>
   );
 }
