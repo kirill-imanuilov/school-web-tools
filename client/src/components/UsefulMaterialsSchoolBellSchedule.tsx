ddea9a0c35ea1006d8a7989d3822d070a67662b2
kirill-imanuilov <kirill.imanuilov@yandex.ru>
2023-12-12 20:12:22 +0300

    feat(`UsefulMaterialsSchoolBellSchedule`):
    - Add a school bell schedule for Saturday
    - Add tabs

--- client/src/components/UsefulMaterialsSchoolBellSchedule.tsx
+++ client/src/components/UsefulMaterialsSchoolBellSchedule.tsx
@@ -173,3 +173,178 @@ export function UsefulMaterialsSchoolBellScheduleMondayFriday() {
     </div>
   );
 }
+export function UsefulMaterialsSchoolBellScheduleSaturday() {
+  return (
+    <div>
+      <div className='useful-materials-school-bell-schedule-container'>
+        <table className='useful-materials-school-bell-schedule-table'>
+          <tr>
+            <th className='useful-materials-school-bell-schedule-th border-top-left-radius'>
+              №
+            </th>
+            <th className='useful-materials-school-bell-schedule-th'>Начало</th>
+            <th className='useful-materials-school-bell-schedule-th border-top-right-radius'>
+              Конец
+            </th>
+          </tr>
+          <tr>
+            <td className='useful-materials-school-bell-schedule-td'>1</td>
+            <td className='useful-materials-school-bell-schedule-td'>08:10</td>
+            <td className='useful-materials-school-bell-schedule-td'>08:50</td>
+          </tr>
+          <tr>
+            <td
+              className='useful-materials-school-bell-schedule-td'
+              colSpan={3}
+            >
+              Перемена 10 минут
+            </td>
+          </tr>
+          <tr>
+            <td className='useful-materials-school-bell-schedule-td'>2</td>
+            <td className='useful-materials-school-bell-schedule-td'>09:00</td>
+            <td className='useful-materials-school-bell-schedule-td'>09:40</td>
+          </tr>
+          <tr>
+            <td
+              className='useful-materials-school-bell-schedule-td'
+              colSpan={3}
+            >
+              Перемена 10 минут
+            </td>
+          </tr>
+          <tr>
+            <td className='useful-materials-school-bell-schedule-td'>3</td>
+            <td className='useful-materials-school-bell-schedule-td'>09:50</td>
+            <td className='useful-materials-school-bell-schedule-td'>10:30</td>
+          </tr>
+          <tr>
+            <td
+              className='useful-materials-school-bell-schedule-td'
+              colSpan={3}
+            >
+              Перемена 10 минут
+            </td>
+          </tr>
+          <tr>
+            <td className='useful-materials-school-bell-schedule-td'>4</td>
+            <td className='useful-materials-school-bell-schedule-td'>10:40</td>
+            <td className='useful-materials-school-bell-schedule-td'>11:20</td>
+          </tr>
+          <tr>
+            <td
+              className='useful-materials-school-bell-schedule-td'
+              colSpan={3}
+            >
+              Перемена 10 минут
+            </td>
+          </tr>
+          <tr>
+            <td className='useful-materials-school-bell-schedule-td'>5</td>
+            <td className='useful-materials-school-bell-schedule-td'>11:30</td>
+            <td className='useful-materials-school-bell-schedule-td'>12:10</td>
+          </tr>
+          <tr>
+            <td
+              className='useful-materials-school-bell-schedule-td'
+              colSpan={3}
+            >
+              Перемена 10 минут
+            </td>
+          </tr>
+          <tr>
+            <td className='useful-materials-school-bell-schedule-td'>6</td>
+            <td className='useful-materials-school-bell-schedule-td'>12:20</td>
+            <td className='useful-materials-school-bell-schedule-td'>13:00</td>
+          </tr>
+          <tr>
+            <td
+              className='useful-materials-school-bell-schedule-td'
+              colSpan={3}
+            >
+              Перемена 10 минут
+            </td>
+          </tr>
+          <tr>
+            <td className='useful-materials-school-bell-schedule-td'>7</td>
+            <td className='useful-materials-school-bell-schedule-td'>13:10</td>
+            <td className='useful-materials-school-bell-schedule-td'>13:50</td>
+          </tr>
+          <tr>
+            <td
+              className='useful-materials-school-bell-schedule-td'
+              colSpan={3}
+            >
+              Перемена 10 минут
+            </td>
+          </tr>
+          <tr>
+            <td className='useful-materials-school-bell-schedule-td'>8</td>
+            <td className='useful-materials-school-bell-schedule-td'>14:00</td>
+            <td className='useful-materials-school-bell-schedule-td'>14:40</td>
+          </tr>
+          <tr>
+            <td
+              className='useful-materials-school-bell-schedule-td'
+              colSpan={3}
+            >
+              Перемена 10 минут
+            </td>
+          </tr>
+          <tr>
+            <td className='useful-materials-school-bell-schedule-td'>9</td>
+            <td className='useful-materials-school-bell-schedule-td'>14:50</td>
+            <td className='useful-materials-school-bell-schedule-td'>15:30</td>
+          </tr>
+          <tr>
+            <td
+              className='useful-materials-school-bell-schedule-td'
+              colSpan={3}
+            >
+              Перемена 10 минут
+            </td>
+          </tr>
+          <tr>
+            <td className='useful-materials-school-bell-schedule-td'>10</td>
+            <td className='useful-materials-school-bell-schedule-td'>15:40</td>
+            <td className='useful-materials-school-bell-schedule-td'>16:20</td>
+          </tr>
+          <tr>
+            <td
+              className='useful-materials-school-bell-schedule-td'
+              colSpan={3}
+            >
+              Перемена 10 минут
+            </td>
+          </tr>
+          <tr>
+            <td className='useful-materials-school-bell-schedule-td'>11</td>
+            <td className='useful-materials-school-bell-schedule-td'>16:30</td>
+            <td className='useful-materials-school-bell-schedule-td'>17:10</td>
+          </tr>
+          <tr>
+            <td
+              className='useful-materials-school-bell-schedule-td'
+              colSpan={3}
+            >
+              Перемена 10 минут
+            </td>
+          </tr>
+          <tr>
+            <td className='useful-materials-school-bell-schedule-td'>12</td>
+            <td className='useful-materials-school-bell-schedule-td'>17:20</td>
+            <td className='useful-materials-school-bell-schedule-td'>18:00</td>
+          </tr>
+          <tr>
+            <td
+              className='useful-materials-school-bell-schedule-td border-bottom-left-radius border-bottom-right-radius'
+              colSpan={3}
+            >
+              Перемена 10 минут
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+}
--- client/src/components/pages/UsefulMaterialsSchoolBellSchedule.tsx
+++ client/src/components/pages/UsefulMaterialsSchoolBellSchedule.tsx
@@ -1,14 +1,26 @@
 import '../../index.css';
 import { BackButton } from '../buttons/BackButton';
 import { UsefulMaterialsSchoolBellScheduleMondayFriday } from '../UsefulMaterialsSchoolBellSchedule';
+import { UsefulMaterialsSchoolBellScheduleSaturday } from '../UsefulMaterialsSchoolBellSchedule';
+import { useState } from 'react';
 
 export function UsefulMaterialsSchoolBellSchedulePage() {
+  const [tabIndex, setTabIndex] = useState(0);
   return (
     <div className='page'>
       <div className='container'>
         <h1 className='container-title'>Расписание звонков</h1>
         <div className='container-content'>
-          <UsefulMaterialsSchoolBellScheduleMondayFriday />
+          <div className='tab-row'>
+            <div className='tab' onClick={() => setTabIndex(0)}>
+              ПН-ПТ {tabIndex === 0 && <div className='tab-selected' />}
+            </div>
+            <div className='tab' onClick={() => setTabIndex(1)}>
+              СБ {tabIndex === 1 && <div className='tab-selected' />}
+            </div>
+          </div>
+          {tabIndex === 0 && <UsefulMaterialsSchoolBellScheduleMondayFriday />}
+          {tabIndex === 1 && <UsefulMaterialsSchoolBellScheduleSaturday />}
           <BackButton />
         </div>
       </div>
